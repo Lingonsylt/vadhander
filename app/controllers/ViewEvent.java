fb4bccbd625e4f8cf15660fcccff2ff3124e02ba
Anton <anton@naetet.se>
2013-04-25 09:22:41 +0200

    view event: basic data is up in template

--- app/controllers/ViewEvent.java
+++ app/controllers/ViewEvent.java
@@ -1,13 +1,38 @@
 package controllers;
 
+import models.Event;
+import models.User;
+import org.joda.time.DateTime;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.viewevent.index;
 
 public class ViewEvent extends Controller {
-  
+    private static Event getFakeUser(int id) {
+        User user = new User();
+        user.name = "username";
+        user.email = "user@name.com";
+
+        Event fakeEvent = new Event();
+        fakeEvent.id = id;
+        fakeEvent.caption = "event #caption";
+        fakeEvent.creator = user;
+        fakeEvent.latitude = 59.4055219f;
+        fakeEvent.longitude = 17.9448913f;
+        fakeEvent.time_created = new DateTime();
+        return fakeEvent;
+    }
+
     public static Result index(int id) {
-        return ok(index.render());
+        //fakeSetup(id);
+        Event event = (Event)Event.find().byId(id);
+
+        // TODO: Remove when create event is done
+        if (id == -1) {
+            event = getFakeUser(-1);
+        }
+
+        return ok(index.render(event));
     }
   
 }
