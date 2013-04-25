ce6a4689436be1bdfda4f43fb1494c51bf408c30
lingon <anton@naetet.se>
2013-04-25 11:31:29 +0200

    added position and comments to ViewEvent

--- app/controllers/ViewEvent.java
+++ app/controllers/ViewEvent.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Comment;
 import models.Event;
 import models.User;
 import org.joda.time.DateTime;
@@ -7,8 +8,10 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.viewevent.index;
 
+import java.util.ArrayList;
+
 public class ViewEvent extends Controller {
-    private static Event getFakeUser(int id) {
+    private static Event getFakeEvent(int id) {
         User user = new User();
         user.name = "username";
         user.email = "user@name.com";
@@ -20,6 +23,11 @@ public class ViewEvent extends Controller {
         fakeEvent.latitude = 59.4055219f;
         fakeEvent.longitude = 17.9448913f;
         fakeEvent.time_created = new DateTime();
+        fakeEvent.comments = new ArrayList<Comment>();
+        Comment comment = new Comment();
+        comment.user = user;
+        comment.text = "This is a fake comment";
+        fakeEvent.comments.add(comment);
         return fakeEvent;
     }
 
@@ -29,7 +37,7 @@ public class ViewEvent extends Controller {
 
         // TODO: Remove when create event is done
         if (id == -1) {
-            event = getFakeUser(-1);
+            event = getFakeEvent(-1);
         }
 
         return ok(index.render(event));
