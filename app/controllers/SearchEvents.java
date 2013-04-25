2c0b40435fc87dac1f75f5f4d739fff3376461b1
danne <danne.collin@gmail.com>
2013-04-25 18:42:08 +0200

    revamped get-method for finding events by tag search

--- app/controllers/SearchEvents.java
+++ app/controllers/SearchEvents.java
@@ -1,40 +1,24 @@
 package controllers;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.RawSql;
-import com.avaje.ebean.RawSqlBuilder;
 import models.Event;
+import models.Tag;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.searchevents.index;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SearchEvents extends Controller {
-
-    public static List<Event> getEventsByTag(List<String> tags) {
-        String sql = "SELECT id, caption, latitude, longitude, time_created, creator_id " +
-                "FROM tag, event" +
-                "WHERE event_id=id AND ";
-        for (String s : tags) {
-            sql+= "text='"+s+"'";
-            if (tags.indexOf(s)!=tags.size()-1) sql+=" OR ";
+    public static Set<Event> getEventsByTag(List<String> tags) {
+        List<Tag> foundTags = Tag.find().where().in("text",tags).findList();
+        Set<Event> foundEvents = new HashSet<Event>();
+        for (Tag t : foundTags) {
+            foundEvents.add(t.event);
         }
-        RawSql rawSql = RawSqlBuilder.unparsed(sql)
-                .columnMapping("id",  "id")
-                .columnMapping("caption",  "caption")
-                .columnMapping("latitude",  "latitude")
-                .columnMapping("longitude",  "longitude")
-                .columnMapping("time_created",  "time_created")
-                .columnMapping("creator_id",  "creator_id")
-                .create();
-
-        com.avaje.ebean.Query<Event> query = Ebean.find(Event.class);
-        query.setRawSql(rawSql);
-
-        return query.findList();
+        return foundEvents;
     }
-
+  
     public static Result index() {
         return ok(index.render());
     }
