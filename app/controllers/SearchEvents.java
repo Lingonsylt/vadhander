8a38885f77c37066c69a433020ecd2c1f07aeb4c
danne <danne.collin@gmail.com>
2013-04-26 10:34:28 +0200

    added method for parsing a string into a list of tag-strings

--- app/controllers/SearchEvents.java
+++ app/controllers/SearchEvents.java
@@ -5,11 +5,21 @@ import models.Tag;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import views.html.viewevent.index;
 
 public class SearchEvents extends Controller {
+
+    public static List<String> stringToTagList(String input) {
+        if (input.equals("") || input.equals("#") || !input.startsWith("#")) throw new IllegalArgumentException("invalid tag");
+        List<String> output = Arrays.asList(input.split("#"));
+        output = output.subList(1,output.size());
+        return output;
+    }
+
     public static Set<Event> getEventsByTag(List<String> tags) {
         List<Tag> foundTags = Tag.find().where().in("text",tags).findList();
         Set<Event> foundEvents = new HashSet<Event>();
