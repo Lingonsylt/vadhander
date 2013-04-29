279f527c48bdac2eb8f27f610a100b8d1b8d663a
Anton <anton@naetet.se>
2013-04-29 15:38:47 +0200

    added styling to ViewEvent.index and added Event.description and Event.road_description

--- app/controllers/ViewEvent.java
+++ app/controllers/ViewEvent.java
@@ -23,11 +23,23 @@ public class ViewEvent extends Controller {
         fakeEvent.latitude = 59.4055219f;
         fakeEvent.longitude = 17.9448913f;
         fakeEvent.time_created = new DateTime();
+
+        fakeEvent.description = "This is a quite short fake event description. The event is gonna be awesome! " +
+                "Yes it is. Yes it is...";
+        fakeEvent.road_description = "Take the subway to the last stop. Then go back again.";
         fakeEvent.comments = new ArrayList<Comment>();
         Comment comment = new Comment();
         comment.user = user;
         comment.text = "This is a fake comment";
         fakeEvent.comments.add(comment);
+
+        Comment comment2 = new Comment();
+        comment2.user = user;
+        comment2.text = "This is a fake comment. This is a fake comment. This is a fake comment. " +
+                "This is a fake comment. This is a fake comment. This is a fake comment. This is a fake comment. " +
+                "This is a fake comment. This is a fake comment. This is a fake comment. This is a fake comment. " +
+                "This is a fake comment. ";
+        fakeEvent.comments.add(comment2);
         return fakeEvent;
     }
 
