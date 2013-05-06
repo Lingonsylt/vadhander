4664d6e13933cd6a4f4c550b371c9cd4c1f0e156
Goodfeather <ledberg.peter@gmail.com>
2013-05-06 09:53:41 +0200

    CreateEvent med större descriptionfält

--- app/models/Tag.java
+++ app/models/Tag.java
@@ -9,9 +9,16 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Tag extends Model {
+    public Tag(String text, Event event){
+        this.text = text;
+        this.event = event;
+    }
     @Id
     public String text;
 
     @ManyToOne(optional = false)
     public Event event;
+    public String toString(){
+        return text;
+    }
 }
